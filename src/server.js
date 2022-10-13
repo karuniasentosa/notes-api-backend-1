7e8baf3759e36b8029091a13edadcfddf211309e
Karunia Sentosa <s32190083@student.ubm.ac.id>
2022-10-13 11:33:55 +0700

    implement user registration

--- src/server.js
+++ src/server.js
@@ -2,12 +2,21 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+
+// === notes
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 
+// === users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UserValidator = require('./validator/users');
+
 const init = async () => {
   const notesService = new NotesService();
+  const userService = new UsersService();
+
   const server = Hapi.server({
     port: process.env.SVPORT,
     host: process.env.SVHOST,
@@ -23,6 +32,14 @@ const init = async () => {
     options: {
       service: notesService,
       validator: NotesValidator,
+    },
+  });
+
+  await server.register({
+    plugin: users,
+    options: {
+      service: userService,
+      validator: UserValidator,
     },
   });
 
